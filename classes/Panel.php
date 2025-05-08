840ed10381c7c6f326884de6a91d65112f69358e
CloudSalander <ruben.as532@gmail.com>
2025-05-08 11:01:03 +0200

    Printing hidden text without newlines

--- classes/Panel.php
+++ classes/Panel.php
@@ -1,7 +1,18 @@
 <?php
 
 class Panel {
+
+    const HIDDEN_CHAR = "_";
+    
     public function __construct(private string $clue, private string $text){}
+    public function show(){
+        //Can we iterate a string as an array with foreach?
+        //Explain why I decide TO NOT convert string to array
+        for($i = 0; $i < strlen($this->text); ++$i) {
+            if($this->text[$i] != " ") echo Panel::HIDDEN_CHAR;
+            else echo " ";
+        }
+    }
 } 
 
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php 
 include('classes/Panel.php');
-
+//TODO: Should be validate this input? Why/Why not?
 $panel1 = new Panel("Es van enamorar en un tren","Before Sunrise");
 $panel2 = new Panel("Lorca","La casa de Bernarda Alba");
 
 print_r($panel1);
 print_r($panel2);
+
+$panel1->show();
+echo PHP_EOL;
+$panel2->show();
 
 
 ?>
