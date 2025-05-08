c41553f562c0d77d984c1235c3c8f5e3c1e43061
CloudSalander <ruben.as532@gmail.com>
2025-05-08 12:28:51 +0200

    Removed unnecessary prints

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ include('classes/Panel.php');
 $panel1 = new Panel("Es van enamorar en un tren","Before Sunrise");
 $panel2 = new Panel("Lorca","La casa de Bernarda Alba");
 
-print_r($panel1);
-print_r($panel2);
-
 $panel1->show();
 echo PHP_EOL;
 $panel2->show();
