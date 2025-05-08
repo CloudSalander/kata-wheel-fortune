abe36e42fdeb904f163362b6208c12ee44aa4f14
CloudSalander <ruben.as532@gmail.com>
2025-05-08 11:07:03 +0200

    now showing hidden text and clues

--- classes/Panel.php
+++ classes/Panel.php
@@ -14,6 +14,7 @@ class Panel {
             else echo " ";
             $currentLineCharsNumber = $this->checkNewLine($currentLineCharsNumber);
         }
+        echo PHP_EOL.$this->clue;
     }
 
     private function checkNewLine(int $charsNumber): int {
