5f7f447ec362d914f59f8afe3fc424ee4f8ed5a9
CloudSalander <ruben.as532@gmail.com>
2025-05-08 10:50:03 +0200

    Now I can create panels :)

--- classes/Panel.php
+++ classes/Panel.php
@@ -1,7 +1,7 @@
 <?php
 
 class Panel {
-    
+    public function __construct(private string $clue, private string $text){}
 } 
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
-include('Panel.php');
+include('classes/Panel.php');
+
+$panel1 = new Panel("Es van enamorar en un tren","Before Sunrise");
+$panel2 = new Panel("Lorca","La casa de Bernarda Alba");
+
+print_r($panel1);
+print_r($panel2);
+
 
 ?>
